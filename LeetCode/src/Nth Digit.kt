8f65d66b90c2492c0e631568830831e25354a28f
최재웅 <dwarf826@stunitas.com>
2020-04-08 09:41:26 +0900

    뇌세척 - 02 중급과제

---
+++ LeetCode/src/Nth Digit.kt
@@ -0,0 +1,20 @@
+
+fun findNthDigit(n: Int): Int {
+    var value = n
+
+    var start = 1
+    var length = 1;
+    var base = 9L;
+
+    while ( value > length * base) {
+        value -= (length * base).toInt()
+        length++
+        start *= 10
+        base *= 10
+    }
+
+    val target = start + (value - 1)/length
+    val index = (value - 1) % length
+
+    return target.toString()[index] - '0'
+}
