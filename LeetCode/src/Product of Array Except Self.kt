65d2d8b21b6a979e58e1cb8c4c3b7e92b5d0a869
최재웅 <dwarf826@stunitas.com>
2020-05-04 17:04:00 +0900

    30 day leetcoding challenge - 3주 1일차

---
+++ LeetCode/src/Product of Array Except Self.kt
@@ -0,0 +1,16 @@
+
+
+private fun productExceptSelf(nums: IntArray): IntArray {
+    val left = IntArray(nums.size)
+    left[0] = 1
+    for(i in 1 until nums.size) {
+        left[i] = nums[i - 1] * left[i - 1]
+    }
+    val right = IntArray(nums.size)
+    right[nums.size - 1] = 1
+    for(i in (nums.size - 2) downTo 0) {
+        right[i] = nums[i + 1] * right[i + 1]
+    }
+
+    return left.mapIndexed { index, i -> i * right[index] }.toIntArray()
+}
